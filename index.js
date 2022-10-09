9fdf68289ae185be25935f3e16320744aa985471
hqahsanul <hqahsanul@gmail.com>
2022-10-09 12:45:54 +0530

    first bot created

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require("express")
+var app = express()
+var bodyParser = require("body-parser")
+const axios = require("axios");
+const API_KEY = "5769150315:AAF7QqCXBYA5cd-CYdXjVMCqNnPeOfshhDM"
+
+app.use(bodyParser.json()) 
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+) 
+
+
+app.post("/new-message", function(req, res) {
+	const { message } = req.body
+
+	
+
+	if (!message || message.text.toLowerCase().indexOf("marco") < 0) {
+		
+		return res.end()
+	}
+
+	
+	axios
+		.post(
+			`https://api.telegram.org/bot${API_KEY}/sendMessage`,
+			{
+				chat_id: message.chat.id,
+				text: "Polo!!",
+			}
+		)
+		.then((response) => {
+			
+			console.log("Message posted")
+			res.end("ok")
+		})
+		.catch((err) => {
+	
+			console.log("Error :", err)
+			res.end("Error :" + err)
+		})
+})
+app.listen(3000, function() {
+	console.log("Telegram app listening on port 3000!")
+})
